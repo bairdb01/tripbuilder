49560858c5375152b7758fefc6371563f0b2c034
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 13:37:49 -0400

    Testing db

--- api.php
+++ api.php
@@ -1,4 +1,9 @@
 <?php
+  $conn_string = "host=ec2-174-129-37-103.compute-1.amazonaws.com port=5432 dbname=d5i884hb69gdln user=diwtzmpjgjqfxk password=m24j2xLEl52xu-sR5W9QMp4A68";
+  $dbconn =  pg_connect ( $conn_string ) or die('Could not connect: ' . pg_last_error());
 
-
+  // Performing SQL query
+  $query = 'SELECT * FROM flights';
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+  echo $result;
 ?>
