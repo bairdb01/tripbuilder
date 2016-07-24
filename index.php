b4fa3d38ef3722d99715037492be2d7cfacce17e
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 17:50:24 -0400

    Testing db functionality

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
   echo "Hello world!";
-  
-  $conn_string = "host=ec2-174-129-37-103.compute-1.amazonaws.com port=5432 dbname=d5i884hb69gdln user=diwtzmpjgjqfxk password=m24j2xLEl52xu-sR5W9QMp4A68";
-  $db =  pg_connect ( $conn_string ) or die('Could not connect: ' . pg_last_error());
 
-  // Performing SQL query
+  $conn_string = "host=ec2-174-129-37-103.compute-1.amazonaws.com port=5432 dbname=d5i884hb69gdln user=diwtzmpjgjqfxk password=m24j2xLEl52xu-sR5W9QMp4A68";
+  $db =  pg_connect ( $conn_string )
+    or die('Could not connect: ' . pg_last_error());
+
+  // Performing an SQL query
   $query = "SELECT * FROM flights";
   $result = pg_query($query)
     or die('Query failed: ' . pg_last_error());
 
-  echo $result;
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+    echo $line;
+  }
 ?>
