5a95eff3faa565ef24d92e9bad6d8dbb754699a6
Ben Baird <ben_baird05@hotmail.com>
2016-07-25 01:54:14 -0400

    Routing

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
   $app = new \Slim\App();
 
-  $app->get('/api/trip/edit/getAirports', function(Request $request, Response $response){
+  $app->get('/api/trip/edit/getAirports', function($request, $response){
     $response = $app->response(200);
     $response["Content-Type"] = "application/json";
     $response->body($db->getAirports());
