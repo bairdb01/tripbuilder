05d22e05b93b85dda8da5a0f6dbcc0c86221896d
Ben Baird <ben_baird05@hotmail.com>
2016-07-25 15:03:30 -0400

    better uris

--- index.php
+++ index.php
@@ -14,68 +14,107 @@
     return $response;
   });
 
-  $app->get('/api/trip/{tripId}/trip', function ($request, $response) use($app){
+  $app->any('/api/trip/{tripId}/trip', function ($request, $response) use($app){
     $db = new DbHandler();
-    $route = $request->getAttribute('route');
-    $tripId = $route->getArgument('tripId');
+    if ($request->getMethod() == 'GET') {
+      // Retrieve the tripname
+      $route = $request->getAttribute('route');
+      $tripId = $route->getArgument('tripId');
 
-    $newHeader = $response->withHeader('Content-type', 'application/json');
-    $body = $response->getBody();
-    $body->write($db->getTripName($tripId));
+      $newHeader = $response->withHeader('Content-type', 'application/json');
+      $body = $response->getBody();
+      $body->write($db->getTripName($tripId));
+
+    } else if ($request->getMethod() == 'PUT') {
+      // Update the tripname
+      $route = $request->getAttribute('route');
+      $tripId = $route->getArgument('tripId');
+      $tripName = $request->getParsedBody()['tripName'];
+
+      $newHeader = $response->withHeader('Content-type', 'application/json');
+      $body = $response->getBody();
+      $body->write($db->setTripName($tripId, $tripName));
+    }
+
     return $response;
   });
 
-  $app->get('/api/trip/{tripId}/flight', function ($request, $response) use($app){
+  $app->any('/api/trip/{tripId}/flight', function ($request, $response) use($app){
     $db = new DbHandler();
-    $route = $request->getAttribute('route');
-    $tripId = $route->getArgument('tripId');
+    if ($response->getMethod() == 'GET') {
+      // Retrieve all flights for a trip
+      $route = $request->getAttribute('route');
+      $tripId = $route->getArgument('tripId');
 
-    $newHeader = $response->withHeader('Content-type', 'application/json');
-    $body = $response->getBody();
-    $body->write($db->getFlights($tripId));
+      $newHeader = $response->withHeader('Content-type', 'application/json');
+      $body = $response->getBody();
+      $body->write($db->getFlights($tripId));
+
+    } else if ($response->getMethod() == 'POST'){
+      // Add a flight to a trip
+      $route = $request->getAttribute('route');
+      $tripId = $route->getArgument('tripId');t
+      $start = $request->getParsedBody()['start'];
+      $dest = $request->getParsedBody()['dest'];
+
+      $newHeader = $response->withHeader('Content-type', 'text/html');
+      $body = $response->getBody();
+      $result = $db->addFlight($tripId, $start, $dest);
+
+    } else if ($response->getMethod() == 'DELETE') {
+      // Delete a flight from a trip
+      $route = $request->getAttribute('route');
+      $tripId = $route->getArgument('tripId');
+      $flightId = $request->getParsedBody()['tripName'];
+
+      $newHeader = $response->withHeader('Content-type', 'application/json');
+      $body = $response->getBody();
+      $body->write($db->removeFlight($tripId, $flightId));
+    }
+
     return $response;
   });
 
 
-  $app->post('api/trip/{tripId}/flight/', function ($request, $response) use($app){
-    $db = new DbHandler();
-    $route = $request->getAttribute('route');
-    $tripId = $route->getArgument('tripId');t
-    $start = $request->getParsedBody()['start'];
-    $dest = $request->getParsedBody()['dest'];
+  // $app->post('api/trip/{tripId}/flight', function ($request, $response) use($app){
+  //   $db = new DbHandler();
+  //   $route = $request->getAttribute('route');
+  //   $tripId = $route->getArgument('tripId');t
+  //   $start = $request->getParsedBody()['start'];
+  //   $dest = $request->getParsedBody()['dest'];
+  //
+  //   $newHeader = $response->withHeader('Content-type', 'text/html');
+  //   $body = $response->getBody();
+  //   $result = $db->addFlight($tripId, $start, $dest);
+  //   if ($result)
+  //     $body->write("True");
+  //   $body->write("False");
+  //   return $response;
+  // });
 
-    $newHeader = $response->withHeader('Content-type', 'text/html');
-    $body = $response->getBody();
-    $result = $db->addFlight($tripId, $start, $dest);
-    if ($result)
-      $body->write("True");
-    $body->write("False");
-    return $response;
-  });
+  // $app->put('/api/trip/{tripId}/trip', function ($request, $response) use($app){
+  //   $db = new DbHandler();
+  //   $route = $request->getAttribute('route');
+  //   $tripId = $route->getArgument('tripId');
+  //   $tripName = $request->getParsedBody()['tripName'];
+  //
+  //   $newHeader = $response->withHeader('Content-type', 'application/json');
+  //   $body = $response->getBody();
+  //   $body->write($db->setTripName($tripId, $tripName));
+  //   return $response;
+  // });
 
-  $app->put('/api/trip/{tripId}/trip', function ($request, $response) use($app){
-    $db = new DbHandler();
-    $route = $request->getAttribute('route');
-    $tripId = $route->getArgument('tripId');
-    $tripName = $request->getParsedBody()['tripName'];
-
-    $newHeader = $response->withHeader('Content-type', 'application/json');
-    $body = $response->getBody();
-    $body->write($db->setTripName($tripId, $tripName));
-    return $response;
-  });
-
-  $app->delete('/api/trip/{tripId}/flight', function ($request, $response) use($app){
-    $db = new DbHandler();
-    $route = $request->getAttribute('route');
-    $tripId = $route->getArgument('tripId');
-    $flightId = $request->getParsedBody()['tripName'];
-
-    $newHeader = $response->withHeader('Content-type', 'application/json');
-    $body = $response->getBody();
-    $body->write($db->removeFlight($tripId, $flightId));
-    return $response;
-  });
+  // $app->delete('/api/trip/{tripId}/flight', function ($request, $response) use($app){
+  //   $db = new DbHandler();
+  //   $route = $request->getAttribute('route');
+  //   $tripId = $route->getArgument('tripId');
+  //   $flightId = $request->getParsedBody()['tripName'];
+  //
+  //   $newHeader = $response->withHeader('Content-type', 'application/json');
+  //   $body = $response->getBody();
+  //   $body->write($db->removeFlight($tripId, $flightId));
+  //   return $response;
+  // });
 
 
 
