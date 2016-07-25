bc087b84ef85836f4bfd3008e208356a9c7182e6
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 23:34:57 -0400

    Testing outputs

--- index.php
+++ index.php
@@ -9,11 +9,16 @@
   echo "</br>";
   echo pg_num_rows($result);
   echo "</br>";
+  echo "<table>";
   while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+    echo "<tr>";
     foreach($line as $col_val){
+      echo "<td>"
       echo $col_val;
-      echo "</br>";
+      echo "</td></br>";
     }
+    echo "</tr>"
   }
+  echo "</table>";
   pg_free_result($result);
 ?>
