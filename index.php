29d4a18a726150518c355797cb978a22ce4375f7
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 23:32:43 -0400

    Testing outputs

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 
   $db = new DbHandler();
   $result = $db->getFlights(3);
-
+  echo </br>;
   echo pg_num_rows($result);
-
+  echo </br>;
   while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
     foreach($line as $col_val){
       echo $col_val;
+      echo </br>;
     }
   }
   pg_free_result($result);
