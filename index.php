b25d3c201326a5b2ba98f4a4fb92051c287ade26
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 23:23:23 -0400

    Testing outputs

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
   echo "Hello World";
 
   $db = new DbHandler();
-  $result = $db->getAirports();
+  $result = $db->getTripName("ben", 3);
+  echo "\n";
   echo pg_num_rows($result);
-
+  echo "\n";
   while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
     foreach($line as $col_val){
       echo $col_val;
+      echo "\n";
     }
   }
   pg_free_result($result);
