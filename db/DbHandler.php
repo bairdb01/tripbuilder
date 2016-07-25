55a10d66d3f1d67d2873b0a8051838814000f790
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 23:55:46 -0400

    Testing

--- db/DbHandler.php
+++ db/DbHandler.php
@@ -39,8 +39,8 @@
     function addFlight($tripId, $start, $dest){
       $row = array(
         "tripId"=>$tripId,
-        "start"=>'$start',
-        "dest"=>'$dest'
+        "start"=>$start,
+        "dest"=>$dest
       );
 
       return pg_insert($this->conn, "flights", $row);
