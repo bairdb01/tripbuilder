67967abb039ab890bcba1f70f10f6ffcf356a68d
Ben Baird <ben_baird05@hotmail.com>
2016-07-25 01:41:27 -0400

    JSON Output

--- index.php
+++ index.php
@@ -2,27 +2,23 @@
   require_once ('db/DbHandler.php');
   require ('vendor/autoload.php');
 
-  echo "Hello World";
-
   $db = new DbHandler();
-  $results = $db->getAirports();
-  echo $results;
 
   $app = new \Slim\App;
 
   $app->get('/api/trip/edit/getAirports', function(Request $request, Response $response){
-    $response = $app->response();
+    $response = $app->response(200);
     $response["Content-Type"] = "application/json";
     $response->body($db->getAirports());
     return $response;
   });
 
   $app->get('/api/trip/edit/{tripId}', function (Request $request, Response $response) {
-    $response = $app->response();
+    $response = $app->response(200);
     $response["Content-Type"] = "application/json";
     $response->body($db->getTripName((int)$args['tripId']));
     return $response;
   });
-  
+
   $app->run();
 ?>
