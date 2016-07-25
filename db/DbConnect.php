dc50d2b078036e09c77d95df0c8b086194fea677
Ben Baird <ben_baird05@hotmail.com>
2016-07-24 22:40:55 -0400

    Syntax Fixes

--- db/DbConnect.php
+++ db/DbConnect.php
@@ -7,6 +7,7 @@ class DbConnnect {
     $conn_string = "host=ec2-174-129-37-103.compute-1.amazonaws.com port=5432 dbname=d5i884hb69gdln user=diwtzmpjgjqfxk password=m24j2xLEl52xu-sR5W9QMp4A68";
     $this->conn = pg_connect ( $conn_string )
       or die('Could not connect: ' . pg_last_error());
+    return $this->conn;
   }
 }
 ?>
