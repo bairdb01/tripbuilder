bcddab831a29cd3e353aa6f57bc1743e334d4429
Ben Baird <ben_baird05@hotmail.com>
2016-07-25 00:41:35 -0400

    Testing

--- index.php
+++ index.php
@@ -5,12 +5,36 @@
   echo "Hello World";
 
   $db = new DbHandler();
-  echo $db->getTripName("ben", 3);
+  $result = $db->getTripName("ben", 3);
+
+  echo "<table>";
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+    echo "<tr>";
+    foreach($line as $col_val){
+      echo "<td>";
+      echo $col_val;
+      echo "</td></br>";
+    }
+    echo "</tr>";
+  }
+  echo "</table>";
 
   $db->removeFlight(3, 6);
 
   $db->updateTripName(3, "New Trip");
-  echo $db->getTripName(3);
+  $result = $db->getTripName(3);
+
+  echo "<table>";
+  while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
+    echo "<tr>";
+    foreach($line as $col_val){
+      echo "<td>";
+      echo $col_val;
+      echo "</td></br>";
+    }
+    echo "</tr>";
+  }
+  echo "</table>";
 
   $result = $db->getFlights(3);
   echo "</br>";
