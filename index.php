b48f3ab2199afb46cce8a904d6c495146e1f7bcf
Ben Baird <ben_baird05@hotmail.com>
2016-07-25 23:21:36 -0400

    API key validation

--- index.php
+++ index.php
@@ -87,6 +87,7 @@
     "error" => function ($request, $response, $arguments) {
         $data["status"] = "error";
         $data["message"] = $arguments["message"];
+        $date["head"] = $request->getHeader()['Authorization'];
         return $response
             ->withHeader("Content-Type", "application/json")
             ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
